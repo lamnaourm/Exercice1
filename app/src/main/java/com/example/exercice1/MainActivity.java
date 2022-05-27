12c7ed0a2006a2c9472c524e9798332cd54a7f68
lamnaourm <mohammed.lamnaour@gmail.com>
2022-05-27 17:31:19 +0100

    Fonction JSON

--- app/src/main/java/com/example/exercice1/MainActivity.java
+++ app/src/main/java/com/example/exercice1/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.exercice1;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public String loadJsonFromRaw(int resId) {
+        String json = "";
+        try {
+            InputStream input = getResources().openRawResource(resId);
+            int taille = 0;
+            taille = input.available();
+            byte[] content = new byte[taille];
+            input.read(content);
+            input.close();
+            json = new String(content);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return json;
     }
 }
